0e8d4517c939664c38f30912b9b830e91c5d0425
Tosu <terebina860@gmail.com>
2024-12-18 01:33:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,8 +61,8 @@ class Config:
     ADMIN = os.environ.get("ADMINS", '6848223695')
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
     API_ID = int(os.environ.get("API_ID", '24620300'))
-    CHAT = int(os.environ.get("CHAT", ""))
-    LOG_GROUP=os.environ.get("LOG_GROUP", "")
+    CHAT = int(os.environ.get("CHAT", "-1002291785414"))
+    LOG_GROUP=os.environ.get("LOG_GROUP", "-1002291785414")
     if LOG_GROUP:
         LOG_GROUP=int(LOG_GROUP)
     else:
@@ -82,10 +82,10 @@ class Config:
     EDIT_TITLE = os.environ.get("EDIT_TITLE", True)
     if EDIT_TITLE == "NO":
         EDIT_TITLE=None
-    DURATION_LIMIT=int(os.environ.get("MAXIMUM_DURATION", 15))
+    DURATION_LIMIT=int(os.environ.get("MAXIMUM_DURATION", 1500))
     DELAY = int(os.environ.get("DELAY", 10))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")     
+    API_HASH = os.environ.get("API_HASH", "24620300")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8155721991:AAG5s1mh3gykXE7JEovgolr7vO2FPhpx6fk")     
     SESSION = os.environ.get("SESSION_STRING", "")
     playlist=[]
     msg = {}
